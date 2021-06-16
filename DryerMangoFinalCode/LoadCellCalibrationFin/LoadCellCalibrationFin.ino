9e94f0459c98442fb6dbe50bc5e075588de8ac21
Kent Johnric Sardina <Sardinakent08@gmail.com>
2021-06-16 17:24:16 +0800

    Load Cell Test While loop

--- DryerMangoFinalCode/LoadCellCalibrationFin/LoadCellCalibrationFin.ino
+++ DryerMangoFinalCode/LoadCellCalibrationFin/LoadCellCalibrationFin.ino
@@ -3,7 +3,8 @@
 #include <Wire.h>
 HX711_ADC LoadCell(9, 10);
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; //Setting up the variables to pins number
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7); 
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+bool moistCon_flagInit = true;
 
 void setup() {
   lcd.begin(16,2);
@@ -14,11 +15,14 @@ void setup() {
 }
 
 void loop() {
-  LoadCell.update();
-  float iWeight = LoadCell.getData();
   lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(iWeight);
-  Serial.print("Weight[g]:");
-  Serial.println(iWeight);
+  while(moistCon_flagInit){
+    LoadCell.update();
+    float iWeight = LoadCell.getData();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(iWeight);
+    Serial.print("Weight[g]:");
+    Serial.println(iWeight);
+  }
 }
