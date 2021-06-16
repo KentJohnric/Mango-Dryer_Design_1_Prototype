953227a13b535bd4d51d4d8ea028d6c6ce7e8401
Kent Johnric Sardina <Sardinakent08@gmail.com>
2021-06-16 17:03:41 +0800

    Test Load Cell with LCD

--- DryerMangoFinalCode/LoadCellCalibrationFin/LoadCellCalibrationFin.ino
+++ DryerMangoFinalCode/LoadCellCalibrationFin/LoadCellCalibrationFin.ino
@@ -1,8 +1,12 @@
+#include <LiquidCrystal.h>
 #include <HX711_ADC.h>
 #include <Wire.h>
 HX711_ADC LoadCell(9, 10);
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; //Setting up the variables to pins number
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); 
 
 void setup() {
+  lcd.begin(16,2);
   LoadCell.begin();
   LoadCell.start(2000);
   LoadCell.setCalFactor(445);
@@ -11,7 +15,10 @@ void setup() {
 
 void loop() {
   LoadCell.update();
-  float i = LoadCell.getData();
+  float iWeight = LoadCell.getData();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(iWeight);
   Serial.print("Weight[g]:");
-  Serial.println(i);
+  Serial.println(iWeight);
 }
